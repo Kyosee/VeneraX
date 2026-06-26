2e3f823d20c96bac908f12e29cd1a4125c64e714
kyosee <kyosee@example.com>
2026-06-26 16:12:39 +0800

    fix: 重命名收藏夹时预填当前名称便于编辑

--- lib/pages/favorites/local_favorites_page.dart
+++ lib/pages/favorites/local_favorites_page.dart
@@ -649,6 +649,7 @@ class _LocalFavoritesPageState extends State<_LocalFavoritesPage> {
                           context: App.rootContext,
                           title: "Rename".tl,
                           hintText: "New Name".tl,
+                          initialValue: widget.folder,
                           onConfirm: (value) {
                             var err = validateFolderName(value.toString());
                             if (err != null) {
